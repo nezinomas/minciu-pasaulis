010fd776b947cee62037213d9a4500384804602e
Audrius <nezinomas@gmail.com>
2018-07-30 14:33:28 +0300

    HomeView testas

---
+++ project/thoughts/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .. import views
+
+class HomeTests(TestCase):
+    def test_home_view_when_empty_db(self):
+        url = reverse('thoughts:home')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Tuščia')
