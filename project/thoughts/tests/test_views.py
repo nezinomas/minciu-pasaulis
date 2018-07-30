0cd7a290d41f19615c472bdfa85aac2ddbdbac8a
Audrius <nezinomas@gmail.com>
2018-07-30 15:44:24 +0300

    Nauji testai

--- project/thoughts/tests/test_views.py
+++ project/thoughts/tests/test_views.py
@@ -5,6 +5,10 @@ from .. import views
 from .. import factories
 
 class HomeTests(TestCase):
+    def test_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.view_class, views.HomeView)
+
     def test_home_view_when_empty_db(self):
         url = reverse('thoughts:home')
         resp = self.client.get(url)
@@ -29,3 +33,13 @@ class HomeTests(TestCase):
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'Mintis pakeliui pasiklydo, bandykite paspausti F5.')
+
+    def test_home_view_with_thought(self):
+        factories.ThoughtsFactory()
+
+        url = reverse('thoughts:home')
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Author')
+        self.assertContains(resp, 'Thought')
