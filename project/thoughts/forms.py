225aa3def7a4fca3dff809ca8c337dc4df83906e
nezinomas <nezinomas@gmai.com>
2023-02-14 15:56:38 +0200

    ThoughtForm

---
+++ project/thoughts/forms.py
@@ -0,0 +1,17 @@
+from crispy_forms.helper import FormHelper
+from django import forms
+
+from ..core.lib.form_helper import set_field_properties
+from . import models
+
+
+class ThoughtForm(forms.ModelForm):
+    class Meta:
+        model = models.Thoughts
+        fields = ['category', 'author', 'thought']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        set_field_properties(self, self.helper)
