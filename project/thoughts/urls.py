158b97b114be9549af4b392a7820e6ff92e24544
nezinomas <nezinomas@gmai.com>
2023-02-14 13:24:01 +0200

    refactoring

--- project/thoughts/urls.py
+++ project/thoughts/urls.py
@@ -1,23 +1,13 @@
-from django.conf import settings
-from django.conf.urls import static
 from django.urls import path
 
 from . import views
+from .apps import App_name
 
-app_name = 'thoughts'
+
+app_name = App_name
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('category/<slug:category>/', views.CategoryView.as_view(), name='category'),
     path('results/', views.SearchView.as_view(), name='search')
 ]
-
-if settings.DEBUG:
-    from django.views.defaults import server_error, page_not_found, permission_denied
-
-    urlpatterns += [
-        path('403/', permission_denied, kwargs={'exception': Exception("Permission Denied")}, name='error403'),
-        path('404/', page_not_found, kwargs={'exception': Exception("Page not Found")}, name='error404'),
-        path('500/', server_error, name='error500'),
-    ]
-    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
